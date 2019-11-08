629e900c8f84caf178f33f78b0d101c70f8b9519
Abdullah Çanakçı <abdullahcanakci.0@gmail.com>
2019-11-08 20:36:34 +0300

    Added EICH and EISCH

---
+++ EICHResolver.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace file_organization
+{
+    class EICHResolver : EISCHResolver
+    {
+        public override bool HasCellar => true;
+    }
+}
---
+++ EISCHResolver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace file_organization
+{
+    class EISCHResolver : CollisionResolver
+    {
+        public override bool HasCellar => false;
+
+        public override void Resolve(int homeAddress, int nodeAddress)
+        {
+            Node home = (Node)base.storage[homeAddress];
+            int nextIndex = home.next;
+            home.next = nodeAddress;
+            Node newNode = (Node)base.storage[nodeAddress];
+            newNode.next = nextIndex;
+        }
+    }
+}
